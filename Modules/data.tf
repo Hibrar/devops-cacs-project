d961febc1e4c536b714200f45a3c8f5f7367d1fa
Hibbah Ibrar <hibbah.ibrar@version1.com>
2025-05-14 09:23:42 +0100

    Fixed data.tf structure

--- Modules/data.tf
+++ Modules/data.tf
@@ -1,33 +1,33 @@
+# ✅ Get the latest Amazon Linux 2023 AMI
 data "aws_ami" "get_ami" {
   most_recent = true
-  owners = ["amazon"]
+  owners      = ["amazon"]
 
   filter {
-    name = "name"
+    name   = "name"
     values = ["al*-ami-2023*-kernel-*-x86_64"]
   }
 
   filter {
-    filter {
-      name = "virtualization-type"
-      values = ["hvm"] #Using hardware virtual machine virtualisation
-    }
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# ✅ Get the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# ✅ Get the subnets in the default VPC
+data "aws_subnets" "public" {
+  filter {
+    name   = "default-for-az"
+    values = ["true"]
   }
 
-  #Default VPC for AWS account
-  data "aws_vpc" "default" {
-    default = true
-  }
-
-  data "aws_subnets" "public" {
-    filter {
-      name = "default-for-az"
-      values = ["true"]
-    }
-
-    filter {
-      name = "vpc-id"
-      values = [data.aws_vpc.default.id]
-    }
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
 }
