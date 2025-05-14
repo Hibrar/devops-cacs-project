82ad34de5d3ef387eb4c23a27fe7f02c1497023f
Hibbah Ibrar <hibbah.ibrar@version1.com>
2025-05-14 09:16:07 +0100

    Fixing closing brace error

--- Modules/data.tf
+++ Modules/data.tf
@@ -1,32 +1,33 @@
 data "aws_ami" "get_ami" {
   most_recent = true
-  owners      = ["amazon"]
+  owners = ["amazon"]
 
   filter {
-    name   = "name"
+    name = "name"
     values = ["al*-ami-2023*-kernel-*-x86_64"]
   }
 
   filter {
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"] #Using hardware virtual machine virtualisation
-  }
-}
-
-#Default VPC for AWS account
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "public" {
-  filter {
-    name   = "default-for-az"
-    values = ["true"]
+    filter {
+      name = "virtualization-type"
+      values = ["hvm"] #Using hardware virtual machine virtualisation
+    }
   }
 
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+  #Default VPC for AWS account
+  data "aws_vpc" "default" {
+    default = true
+  }
+
+  data "aws_subnets" "public" {
+    filter {
+      name = "default-for-az"
+      values = ["true"]
+    }
+
+    filter {
+      name = "vpc-id"
+      values = [data.aws_vpc.default.id]
+    }
   }
 }
