74849045f3b5bdb57d253d641db9c13d62f71467
Hibbah Ibrar <hibbah.ibrar@version1.com>
2025-05-15 14:22:06 +0100

    Changes to mongodb.tf code

--- Modules/mongodb.tf
+++ Modules/mongodb.tf
@@ -2,9 +2,9 @@ data "http" "my_ip" {
   url = "https://checkip.amazonaws.com/"
 }
 
-# data "aws_ssm_parameter" "amazon_linux_ami" {
-#   name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
-# }
+data "aws_ssm_parameter" "amazon_linux_ami" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
 
 resource "aws_security_group" "mongodb_sg" {
   name        = "mongodb-sg"
