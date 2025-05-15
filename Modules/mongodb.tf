f6df44d364619d02781d90fdd395b83e45657e0c
Hibbah Ibrar <hibbah.ibrar@version1.com>
2025-05-15 23:46:01 +0100

    Demo Run Test

--- Modules/mongodb.tf
+++ Modules/mongodb.tf
@@ -2,8 +2,11 @@ data "http" "my_ip" {
   url = "https://checkip.amazonaws.com/"
 }
 
+# data "aws_ssm_parameter" "amazon_linux_ami" {
+#   name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+# }
 data "aws_ssm_parameter" "amazon_linux_ami" {
-  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+  name = "/aws/service/ami-amazon-linux-2/latest/image_id"
 }
 
 resource "aws_security_group" "mongodb_sg" {
